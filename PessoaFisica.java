0d9eb7ed122fa23028894a3465a8cb13d1eb8862
Meire-Rosa <meirematematica@gmail.com>
2022-03-28 22:11:35 -0300

    Adicionando aulas mrbank

---
+++ PessoaFisica.java
@@ -0,0 +1,36 @@
+package mrbank;
+
+public class PessoaFisica extends PessoaHeranca{//gcuf
+	private String cpf;
+	private String rg;
+	private String profissao;
+	
+	public String getCpf() {//gcuf
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getProfissao() {
+		return profissao;
+	}
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+	public PessoaFisica(String cpf, String rg, String profissao) {
+		super();
+		this.cpf = cpf;
+		this.rg = rg;
+		this.profissao = profissao;
+	}
+	
+	
+	
+}
---
+++ TestaBanco.java
@@ -0,0 +1,69 @@
+package mrbank;
+
+public class TestaBanco {
+
+	public static void main(String[] args) {
+		
+//		Pessoa pessoaNumero1 = new Pessoa(); //Criar uma instância = copia
+//		pessoaNumero1.nome = "Maria do Bairro";
+//		pessoaNumero1.profissao = "Agricultora";
+//		pessoaNumero1.cpf = "643.829.641-48";
+//		System.out.println("Nome: "+pessoaNumero1.nome);
+//		System.out.println("Profissão: "+pessoaNumero1.profissao);
+//		System.out.println("CPF: "+pessoaNumero1.cpf);
+//				
+//		PessoaNumero1.rg
+//		
+//		
+//		Pessoa pessoaNumero2 = pessoaNumero1;
+//		pessoaNumero2.nome = "João dos Tomates";
+//		System.out.println("========================");
+//		System.out.println("Nome: "+pessoaNumero2.nome);
+//		System.out.println("Nome: "+ pessoaNumero1.nome);
+//		
+//		Pessoa pessoaNova = new Pessoa(); //Instanciar a classe pessoa
+//		pessoaNova.setNome("José das Couves");
+//		System.out.println("Nome: "+pessoaNova.getNome());
+		
+//		Pessoa pessoaComConstrutor1 = new Pessoa("João dos Tomates", "259.800.966-55", "123,456-78");
+		
+//		System.out.println("Nome "+pessoaComConstrutor1.getNome());
+//		System.out.println("CPF: " +pessoaComConstrutor1.getCpf());
+//		System.out.println("RG: "+ pessoaComConstrutor1.getRg());
+		
+		
+		Conta conta1 = new Conta();
+		conta1.setAgencia(1234);
+		conta1.setNumeroConta(7889);
+		//conta1.setSaldo(10000); >>Não se deve operar diretamente esta infor
+		conta1.setTitular("MeireRosa");
+		conta1.depositar(100000);
+		
+		
+		
+		
+		
+		System.out.println("Agência: "+conta1.getAgencia());
+		System.out.println("Conta: "+conta1.getNumeroConta());
+		System.out.println("Saldo: "+ conta1.getSaldo());
+		System.out.println("Titular: "+conta1.getTitular());
+		System.out.printf("Saldo Atual: R$%.2f\n ",+conta1.getSaldo());
+		
+		
+		Conta conta2 = new Conta();//Aqui pode criar com o método construtor
+		conta2.setAgencia(5582);
+		conta2.setNumeroConta(2234);
+		
+		conta1.transferir(10000, conta2);
+		
+		conta1.sacar(20);
+		
+		//if(conta1.sacar(20)) {
+			//System.out.println();
+		//}
+		
+		
+		System.out.println("Saldo Atual Meire: R$ "+conta1.getSaldo());
+		System.out.println("Saldo Atual Marcos: R$ "+conta2.getSaldo());
+	}
+}
